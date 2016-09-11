10713ce7648d0fd04d6843b02eeb0b9289bcbb21
susufqx <jiangsulirui@gmail.com>
2016-09-11 23:35:24 +0200

    Add the semantic-ui accounts

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,7 +1,27 @@
-Accounts.ui.config({
-  passwordSignupFields:"USERNAME_ONLY",
-  forceEmailLowercase: true,
-  forceUsernameLowercase: true,
-  forcePasswordLowercase: true
+// Options
+AccountsTemplates.configure({
+    //defaultLayout: 'emptyLayout',
+    showForgotPasswordLink: true,
+    overrideLoginErrors: true,
+    enablePasswordChange: true,
+    sendVerificationEmail: false,
+
+    //enforceEmailVerification: true,
+    //confirmPassword: true,
+    //continuousValidation: false,
+    //displayFormLabels: true,
+    //forbidClientAccountCreation: false,
+    //formValidationFeedback: true,
+    //homeRoutePath: '/',
+    //showAddRemoveServices: false,
+    //showPlaceholders: true,
+
+    negativeValidation: true,
+    positiveValidation:true,
+    negativeFeedback: false,
+    positiveFeedback:true,
+
+    // Privacy Policy and Terms of Use
+    //privacyUrl: 'privacy',
+    //termsUrl: 'terms-of-use',
 });
-accountsUIBootstrap3.setLanguage('zh-CN');
